7efd0748c8db1f72a23d09eba25ee1867cee425c
Charlton Trezevant <ct@ctis.me>
2020-06-06 21:03:57 -0400

    Update to latest debugf

--- mmgr/lib/debugf.h
+++ mmgr/lib/debugf.h
@@ -2,19 +2,21 @@
 // (c) Charlton Trezevant - 2018
 #include <stdio.h>
 
+// Global debug levels
 #define DEBUG_LEVEL_ALL 0
-#define DEBUG_LEVEL_MMGR 1
-#define DEBUG_LEVEL_LOGIC 2
-#define DEBUG_LEVEL_STATE 3
-#define DEBUG_LEVEL_INFO 4
-#define DEBUG_LEVEL_NONE 99
+#define DEBUG_LEVEL_TRACE 1
+#define DEBUG_LEVEL_INFO 2
 
+// Functionality-specific debug levels
+#define DEBUG_TRACE_MMGR -7
+
+// Current debug level set/disable
 #define DEBUG DEBUG_LEVEL_ALL
 
 #ifdef DEBUG
 #define debugf(lvl, fmt, ...) \
         ({ \
-                if (DEBUG == 0 || (lvl) >= DEBUG) { \
+                if (DEBUG == 0 || (lvl > 0 && lvl >= DEBUG) || (lvl < 0 && lvl == DEBUG)) { \
                         fprintf(stderr, fmt, ## __VA_ARGS__); fflush(stderr); \
                 } \
         })
